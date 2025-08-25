f9637f2b708fef1a91d40b1916695ba9d0fcfddc
Megan Jakims <meganjakims1@gmail.com>
2025-08-25 10:01:20 -0400

    1st submit project 15

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-export const baseUrl = "http://localhost:3001";
+export const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.wtwr.fin-tech.com"
+    : "http://localhost:3001";
 
 function checkResponse(res) {
   return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
